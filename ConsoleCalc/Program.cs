37ee62829c414aee13849e4fb54cf0243d9b6d3f
alstof <alstof@gmail.com>
2018-10-08 11:48:47 -0700

    Revert "Add methods skeletons"
    This reverts commit 6528931e4257a6db7ddba28177138be42d294cbf.

--- ConsoleCalc/Program.cs
+++ ConsoleCalc/Program.cs
@@ -6,23 +6,8 @@ namespace ConsoleCalc
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter arithmetic expression:");
-            string input = getUserInput();
-            Console.WriteLine(getCalcResult(input));
-            Console.ReadLine();
-        }
-        static string getUserInput()
-        {
-            return default(String);
-        }
+            Console.WriteLine("Hello World!");
 
-        static int getCalcResult(string expression)
-        {
-            return default(int);
         }
-        static int Add(int a, int b) { return a + b; }
-        static int Sub(int a, int b) { return a - b; }
-        static int Mul(int a, int b) { return a * b; }
-        static int Div(int a, int b) { return a / b; }
     }
 }
