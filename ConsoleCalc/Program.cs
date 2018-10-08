1bff93b575809f18b443ad03bb6ea11ef0e177eb
alstof <alstof@gmail.com>
2018-10-08 14:47:28 -0700

    Fix methods naming

--- ConsoleCalc/Program.cs
+++ ConsoleCalc/Program.cs
@@ -8,18 +8,18 @@ namespace ConsoleCalc
         static void Main(string[] args)
         {
             Console.WriteLine("Enter arithmetic expression (ex. 2 + 3):");
-            Console.WriteLine(getUserInputAndCalc());
+            Console.WriteLine(GetUserInputAndCalc());
             Console.ReadLine();
         }
-        static int getUserInputAndCalc()
+        static int GetUserInputAndCalc()
         {
             string userInput = default(string);
             do
             {
                 userInput = Console.ReadLine();
-            } while (!isValid(userInput));
-            int[] numbers = convertToInt(getOperands(userInput));
-            switch (getAction(userInput))
+            } while (!IsValid(userInput));
+            int[] numbers = ConvertToInt(GetOperands(userInput));
+            switch (GetAction(userInput))
             {
                 case "+": return Add(numbers[0], numbers[1]);
                 case "-": return Sub(numbers[0], numbers[1]);
@@ -29,36 +29,36 @@ namespace ConsoleCalc
             return default(int);
         }
 
-        static int[] convertToInt(string[] operands)
+        static int[] ConvertToInt(string[] operands)
         {
-            return new int[] {Int32.Parse(operands[0]), Int32.Parse(operands[1])};
+            return new int[] { Int32.Parse(operands[0]), Int32.Parse(operands[1]) };
         }
-        public static string cleanUpInput (string expression)
+        public static string CleanUpInput(string expression)
         {
             Regex regex = new Regex(@"\s+");
             return regex.Replace(expression, "");
         }
-        static string[] getOperands(string expression)
+        static string[] GetOperands(string expression)
         {
-            return cleanUpInput(expression).Split('+', '-', '/', '*');
+            return CleanUpInput(expression).Split('+', '-', '/', '*');
         }
-        public static string getAction(string expression)
+        public static string GetAction(string expression)
         {
             Regex regex = new Regex(@"[\+\-\\\*]");
             foreach (Match match in regex.Matches(
-                cleanUpInput(expression)
+                CleanUpInput(expression)
                 ))
             {
                 return match.Value;
             }
             return string.Empty;
         }
-        static bool isValid(string str)
+        static bool IsValid(string str)
         {
-            string[] numbers = getOperands(str);
+            string[] numbers = GetOperands(str);
             int value = default(int);
             bool isEnoughOperands = numbers.Length == 2;
-            bool isValidAction = getAction(str) != string.Empty;
+            bool isValidAction = GetAction(str) != string.Empty;
             bool isValidIntegers = Int32.TryParse(numbers[0], out value) && Int32.TryParse(numbers[1], out value);
             if (!isValidAction || !isValidIntegers || !isEnoughOperands)
             {
@@ -67,14 +67,15 @@ namespace ConsoleCalc
             return isValidIntegers && isValidAction && isEnoughOperands;
         }
 
-        static int getCalcResult(string expression)
+        static int GetCalcResult(string expression)
         {
             return default(int);
         }
         static int Add(int a, int b) { return a + b; }
         static int Sub(int a, int b) { return a - b; }
         static int Mul(int a, int b) { return a * b; }
-        static int Div(int a, int b) {
+        static int Div(int a, int b)
+        {
             if (b == 0) return 0;
             return a / b;
         }
